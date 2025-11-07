0a9dbc27d6217503bc45404ca0f3501cae3ef484
Selasie5 <selasisepenu5@gmail.com>
2025-11-07 19:18:39 +0000

    Update metadata in layout component for improved clarity and detail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,11 @@ import BackgroundSoundController from "@/components/core/BackgroundSoundControll
 
 
 export const metadata: Metadata = {
-  title: "Selasie Sepenu - Software Engineer",
-  description: "Personal portfolio of Selasie Sepenu, a software engineer.",
+  title: "Selasie Sepenu | Software Engineer",
+  description: " Selasie is a software engineer passionate about crafting reliable systems and shaping how technology connects people.Over the past few years, I’ve worked across startups and engineering teams — building tools, mentoring peers, and learning from some of the best.",
+  icons: [
+    { rel: "icon", url: "/assets/project.png", type: "image/png" },
+  ],
 };
 
 export default function RootLayout({
